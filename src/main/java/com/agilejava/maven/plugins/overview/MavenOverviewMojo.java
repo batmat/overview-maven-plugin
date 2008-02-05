fe90f3b59d8b128440172fe03ff7e8827104bbbd
Hubert Iwaniuk <neotyk@kungfoo.pl>
2008-02-05 22:43:02 +0000

    Converted plugin to work as report
    git-svn-id: https://maven-overview-plugin.googlecode.com/svn/trunk@11 f6411582-9c43-0410-99a3-031e62b87593

--- src/main/java/com/agilejava/maven/plugins/overview/MavenOverviewMojo.java
+++ src/main/java/com/agilejava/maven/plugins/overview/MavenOverviewMojo.java
@@ -6,17 +6,18 @@ import com.agilejava.maven.plugins.overview.render.MyVertexPaintFunction;
 import com.agilejava.maven.plugins.overview.render.MyVertexShapeFunction;
 import com.agilejava.maven.plugins.overview.render.MyVertexStringer;
 import edu.uci.ics.jung.graph.DirectedGraph;
-import edu.uci.ics.jung.visualization.*;
+import edu.uci.ics.jung.visualization.PluggableRenderer;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.contrib.KKLayout;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactCollector;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.doxia.sink.Sink;
+import org.apache.maven.doxia.siterenderer.Renderer;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
 import org.apache.maven.shared.dependency.tree.DependencyTreeBuilder;
 
 import javax.imageio.ImageIO;
@@ -25,19 +26,20 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
- * MavenOverviewPlugin.
+ * Maven Overview Plugin.
  * <p/>
- * Generates graph of project dependencies.
+ * Generates overview graph of project dependencies.
  *
  * @author Wilfred Springer
  * @author Hubert Iwaniuk
- * @goal generate
+ * @goal overview
+ * @phase site
  */
-public class MavenOverviewPlugin extends AbstractMojo {
+public class MavenOverviewMojo extends AbstractMavenReport {
 
     /**
      * Artifacts to be excluded. Regular expressions matching the full artifact
@@ -49,23 +51,20 @@ public class MavenOverviewPlugin extends AbstractMojo {
 
     /**
      * Rendered graph width in pixels.
-     * Default value: 400
      *
-     * @parameter expression="${width}" default-value="400"
+     * @parameter expression="${width}" default-value="1200"
      */
-    private int width = 400;
+    private int width = 1200;
 
     /**
      * Rendered graph height in pixels.
-     * Default value: 400
      *
-     * @parameter expression="${height}" default-value="400"
+     * @parameter expression="${height}" default-value="1200"
      */
-    private int height = 400;
+    private int height = 1200;
 
     /**
      * Should vertex name be full artifact ID.
-     * Default value: false
      *
      * @parameter expression="${vertexFullLabel}" default-value="false"
      */
@@ -82,6 +81,15 @@ public class MavenOverviewPlugin extends AbstractMojo {
     private String suppressedScopes;
 
     /**
+     * Directory where reports will go.
+     *
+     * @parameter expression="${project.reporting.outputDirectory}"
+     * @required
+     * @readonly
+     */
+    private String outputDirectory;
+
+    /**
      * The projects in the reactor for aggregation report.
      * <p/>
      * Note: This is passed by Maven and must not be configured by the user.
@@ -91,14 +99,6 @@ public class MavenOverviewPlugin extends AbstractMojo {
      * @readonly
      */
     private List reactorProjects;
-
-    /**
-     * File to save rendered graph to.
-     * Default value: ${basedir}/target/${project.artifactId}.png
-     *
-     * @parameter expression="${basedir}/target/${project.artifactId}.png"
-     */
-    private File outputFile;
 
     /**
      * Maven Project.
@@ -122,52 +122,17 @@ public class MavenOverviewPlugin extends AbstractMojo {
      * @required
      * @readonly
      */
-    protected ArtifactCollector artifactCollector;
+    private ArtifactCollector artifactCollector;
 
     /**
-     * Artifact resolver, needed to download source jars for inclusion in
-     * classpath.
-     *
-     * @component role="org.apache.maven.artifact.resolver.ArtifactResolver"
-     * @required
-     * @readonly
-     */
-    protected ArtifactResolver artifactResolver;
-
-    /**
-     * Artifact factory, needed to download source jars for inclusion in
-     * classpath.
-     *
-     * @component role="org.apache.maven.artifact.factory.ArtifactFactory"
-     * @required
-     * @readonly
-     */
-    protected ArtifactFactory artifactFactory;
-
-    /**
-     * @component role="org.apache.maven.artifact.metadata.ArtifactMetadataSource"
-     * hint="maven"
+     * @component role="org.apache.maven.artifact.metadata.ArtifactMetadataSource" hint="maven"
      */
     protected ArtifactMetadataSource artifactMetadataSource;
 
     /**
-     * Remote repositories which will be searched for source attachments.
-     *
-     * @parameter expression="${project.remoteArtifactRepositories}"
-     * @required
-     * @readonly
-     */
-    protected List remoteArtifactRepositories;
-
-    /**
      * @component
      */
-    private ArtifactCollector collector;
-
-    /**
-     * @component
-     */
-    protected ArtifactFactory factory;
+    protected ArtifactFactory artifactFactory;
 
     /**
      * @component
@@ -175,19 +140,94 @@ public class MavenOverviewPlugin extends AbstractMojo {
     private DependencyTreeBuilder dependencyTreeBuilder;
 
     /**
+     * Generates the site report.
+     *
+     * @component
+     */
+    private Renderer siteRenderer;
+
+    private static final String BUNDLE_NAME = "overview";
+    private static final String DESCRIPTION = "report.overview.description";
+    private static final String NAME = "report.overview.name";
+    private static ResourceBundle info = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
+    /**
      * Plugin Information
      */
-    private static ResourceBundle info = ResourceBundle.getBundle("plugin", Locale.getDefault());
     private String pluginName = info.getString("plugin.name");
     private String pluginVersion = info.getString("plugin.version");
     private String pluginBuilder = info.getString("plugin.buildBy");
 
-    public MavenOverviewPlugin() {
+    public MavenOverviewMojo() {
         suppressedScopes = "compile";
     }
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    protected Renderer getSiteRenderer() {
+        return siteRenderer;
+    }
+
+    protected String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    protected MavenProject getProject() {
+        return project;
+    }
+
+    public String getOutputName() {
+        return "overview";
+    }
+
+    public String getName(Locale locale) {
+        return getBundle(locale).getString(NAME);
+    }
+
+    public String getDescription(Locale locale) {
+        return getBundle(locale).getString(DESCRIPTION);
+    }
+
+    private ResourceBundle getBundle(Locale locale) {
+        return ResourceBundle.getBundle(BUNDLE_NAME, locale, this.getClass().getClassLoader());
+    }
+
+    /**
+     * Here goes execution of report.
+     *
+     * @param locale Current locale.
+     * @throws MavenReportException Say no more.
+     */
+    protected void executeReport(Locale locale) throws MavenReportException {
+        Sink sink = getSink();
+        sink.head();
+        sink.title();
+        sink.text(getName(locale));
+        sink.title_();
+        sink.head_();
+
+        sink.body();
+        sink.section1();
+
+        sink.sectionTitle1();
+        sink.text("Dependency Overview Graph for " + getProject().getName());
+        sink.sectionTitle1_();
+        sink.lineBreak();
+        sink.lineBreak();
+        generateOverview();
+        sink.figure();
+        sink.figureGraphics(getGraphLocationInSite());
+        sink.figureCaption();
+        sink.text("Dependency Overview Graph");
+        sink.figureCaption_();
+        sink.figure_();
+        sink.lineBreak();
+        sink.section1_();
+        sink.body_();
+        sink.flush();
+        sink.close();
+    }
+
+    private void generateOverview() {
         getLog().debug(pluginName + " v" + pluginVersion + " build by " + pluginBuilder);
+        File outputFile = new File(getGraphLocation());
         if (!outputFile.exists()) {
             getLog().debug("Creating outputFile: " + outputFile.getAbsolutePath());
             outputFile.getParentFile().mkdirs();
@@ -199,13 +239,13 @@ public class MavenOverviewPlugin extends AbstractMojo {
                 excludes,
                 dependencyTreeBuilder,
                 localRepository,
-                factory,
+                artifactFactory,
                 artifactMetadataSource,
-                collector,
+                artifactCollector,
                 this);
 
         getLog().debug("Generating graph");
-        DirectedGraph graph = dependencyProcessor.createGraph(project, reactorProjects);
+        DirectedGraph graph = dependencyProcessor.createGraph(getProject(), reactorProjects);
 
         getLog().debug("Rendering graph");
         KKLayout layout = new KKLayout(graph);
@@ -241,5 +281,13 @@ public class MavenOverviewPlugin extends AbstractMojo {
         renderer.setVertexLabelCentering(true);
         renderer.setEdgeStringer(new MyEdgeStringer(suppressedScopes));
         return renderer;
+    }
+
+    private String getGraphLocation() {
+        return getOutputDirectory() + File.separator + getGraphLocationInSite();
+    }
+
+    public String getGraphLocationInSite() {
+        return "images" + File.separator + getOutputName() + ".png";
     }
 }
