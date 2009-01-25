74a9f04b354114deb14dca181cdb9f3c68eaade8
Hubert Iwaniuk <neotyk@kungfoo.pl>
2009-01-25 17:22:24 +0000

    Issue 14: preparation for upload to central
    git-svn-id: https://maven-overview-plugin.googlecode.com/svn/trunk@189 f6411582-9c43-0410-99a3-031e62b87593

--- src/it/multi-test/validate.groovy
+++ src/it/multi-test/validate.groovy
@@ -1,4 +1,4 @@
-def file = new File(basedir, 'target/site/images/overview.png')
+def file = new File('target/site/images/overview.png')
 
 log.debug("Checking if $file exists.")
 assert file.exists() : "$file doesn't exist"
